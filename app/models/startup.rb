2604962de54e0f0672f4a8afead564d549897775
Huseyin Cicek <htcicek@users.noreply.github.com>
2019-09-19 17:02:07 -0400

    Startup basic class built and tested

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,40 @@
 class Startup
+  attr_accessor :name
+  attr_reader :founder, :domain
+  
+  @@all = []
 
-end
+  # Class methods
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_founder(founder)
+    self.all.find { |startup| startup.founder == founder }
+  end
+
+  def self.domains
+    self.all.map { |startup| startup.domain }.uniq
+  end
+  
+  # Instance methods
+
+  def pivot(domain, name)
+    @domain = domain
+    @name = name
+  end
+
+
+
+
+
+
+end #end startup
