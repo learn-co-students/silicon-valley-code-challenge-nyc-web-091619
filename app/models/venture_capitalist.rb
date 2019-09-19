5c550afc985ff25c2c3e139bcfc0a90079f7b276
Huseyin Cicek <htcicek@users.noreply.github.com>
2019-09-19 17:07:31 -0400

    Venture Capitalist basics built and tested

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,22 @@
 class VentureCapitalist
+  attr_accessor :name, :total_worth
 
-end
+  @@all = []
+
+  # Class methods
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club
+    self.all.select {|vc| vc.total_worth > 1000000000 }
+  end
+
+end # END venture capitalist
