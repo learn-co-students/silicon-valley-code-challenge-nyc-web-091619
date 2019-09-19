6a8a4e0cfe8e0f859953dc0efc6b48adcae37fc8
Huseyin Cicek <htcicek@users.noreply.github.com>
2019-09-19 17:23:34 -0400

    Funding Round basics built and tested

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,19 @@
 class FundingRound
+  attr_reader :startup, :venture_capitalist
+  attr_accessor :type, :investment
 
+  @@all = []
+
+  def initialize(startup, vc, type, investment)
+    @startup = startup
+    @venture_capitalist = vc
+    @type = type
+    @investment = (investment > 0) ? (investment.to_f) : (0.0)
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
