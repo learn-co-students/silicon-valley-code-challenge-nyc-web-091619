f65476d30a63c8a2853a5dfe37d9037254facf93
Huseyin Cicek <htcicek@users.noreply.github.com>
2019-09-19 18:06:26 -0400

    Finished everything except VentureCapitalist#invested

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -19,4 +19,27 @@ class VentureCapitalist
     self.all.select {|vc| vc.total_worth > 1000000000 }
   end
 
+  # Instance methods
+  def offer_contract(startup, type, investment)
+    FundingRound.new(startup, self, type, investment)
+  end
+
+  def funding_rounds
+    FundingRound.all.select {|fr| fr.venture_capitalist == self}
+  end
+
+  def portfolio
+    self.funding_rounds.map {|fr| fr.startup }.uniq
+  end
+
+  def biggest_investment
+    self.funding_rounds.max_by {|fr| fr.investment}
+  end
+
+  def invested(domain)
+    binding.pry
+    doms = self.funding_rounds.select {|fr| Startup.domain == domain}
+    doms.map{|fr| fr.investment}.reduce{|sum, investment| sum + investment}
+    
+  end
 end # END venture capitalist
